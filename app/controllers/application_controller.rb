646b2a228ecb71cfa4573fc7dacf9c61c2e47af4
Martin Fenner <martin.fenner@datacite.org>
2017-04-18 22:05:06 +0200

    small bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,20 +7,18 @@ class ApplicationController < ActionController::API
                           ActionController::UnpermittedParameters,
                           NoMethodError]
 
-  unless Rails.env.test? || Rails.env.development?
-    rescue_from *RESCUABLE_EXCEPTIONS do |exception|
-      status, message = case exception.class.to_s
-                        when "AbstractController::ActionNotFound", "ActionController::RoutingError"
-                          [404, "The resource you are looking for doesn't exist."]
-                        when "ActiveModel::ForbiddenAttributesError", "ActionController::UnpermittedParameters", "ActionController::ParameterMissing"
-                          [422, exception.message]
-                        when "NoMethodError"
-                          Rails.env.development? || Rails.env.test? ? [422, exception.message] : [422, "The request could not be processed."]
-                        else
-                          [400, exception.message]
-                        end
+  rescue_from *RESCUABLE_EXCEPTIONS do |exception|
+    status, message = case exception.class.to_s
+                      when "AbstractController::ActionNotFound", "ActionController::RoutingError"
+                        [404, "The resource you are looking for doesn't exist."]
+                      when "ActiveModel::ForbiddenAttributesError", "ActionController::UnpermittedParameters", "ActionController::ParameterMissing"
+                        [422, exception.message]
+                      when "NoMethodError"
+                        Rails.env.development? || Rails.env.test? ? [422, exception.message] : [422, "The request could not be processed."]
+                      else
+                        [400, exception.message]
+                      end
 
-      render plain: message, status: status, content_type: "text/plain"
-    end
+    render plain: message, status: status, content_type: "text/plain"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :heartbeat, only: [:index]
 
   # workaround for fetching accept header in path
-  get '/application/*application_accept/:id', to: 'index#show', constraints: { id: /.*/, format: false }
-  get '/text/*text_accept/:id', to: 'index#show', constraints: { id: /.*/, format: false }
-  resources :index, path: '/', only: [:show, :index], constraints: { id: /.*/, format: false }
+  get '/application/*application_accept/:id', to: 'index#show', constraints: { id: /.+/, format: false }
+  get '/text/*text_accept/:id', to: 'index#show', constraints: { id: /.+/, format: false }
+  resources :index, path: '/', only: [:show, :index], constraints: { id: /.+/, format: false }
 
   root :to => 'index#index'
 
