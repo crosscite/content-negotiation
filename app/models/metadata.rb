ed4a3b2574ea0193167636772f5a15ef0855435c
Kristian Garza <kj.garza@gmail.com>
2017-05-30 12:09:32 +0200

    skip memcached on xml

--- app/models/metadata.rb
+++ app/models/metadata.rb
@@ -1,16 +1,18 @@
 class Metadata < Bolognese::Metadata
   # fetching and parsing is an expensive step, so we cache using the crosscite format
-  def initialize(input: nil)
-    return super(input: input)
+  def initialize(input: nil, from: nil, format: nil)
+    # don't cache if passing thru metadata or when in Test mode
+    return super(input: input, from: from) if from == format.to_s || %w(test).include?(Rails.env)
+    # return super(input: input)
     # return super(input: input) if %w(test).include?(Rails.env)
-    #
-    # cached_input = Rails.cache.read(input)
-    #
-    # if cached_input
-    #   super(input: cached_input, from: "crosscite")
-    # else
-    #   super(input: input)
-    #   Rails.cache.write(input, self.crosscite, raw: true)
-    # end
+
+    cached_input = Rails.cache.read(input)
+
+    if cached_input
+      super(input: cached_input, from: "crosscite")
+    else
+      super(input: input)
+      Rails.cache.write(input, self.crosscite, raw: true)
+    end
   end
 end
