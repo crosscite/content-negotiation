b26fc919a47f1712d428836db1973035649d9fce
Kristian Garza <kj.garza@gmail.com>
2017-05-30 09:03:26 +0200

    removed memcached

--- app/models/metadata.rb
+++ app/models/metadata.rb
@@ -1,15 +1,16 @@
 class Metadata < Bolognese::Metadata
   # fetching and parsing is an expensive step, so we cache using the crosscite format
   def initialize(input: nil)
-    return super(input: input) if %w(test).include?(Rails.env)
-
-    cached_input = Rails.cache.read(input)
-
-    if cached_input
-      super(input: cached_input, from: "crosscite")
-    else
-      super(input: input)
-      Rails.cache.write(input, self.crosscite, raw: true)
-    end
+    return super(input: input)
+    # return super(input: input) if %w(test).include?(Rails.env)
+    #
+    # cached_input = Rails.cache.read(input)
+    #
+    # if cached_input
+    #   super(input: cached_input, from: "crosscite")
+    # else
+    #   super(input: input)
+    #   Rails.cache.write(input, self.crosscite, raw: true)
+    # end
   end
 end
