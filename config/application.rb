dfaba0a7e7d301fd3d5eac31c55c8a9a25f83096
Martin Fenner <mfenner@datacite.org>
2017-09-11 15:02:27 +0200

    tweak caching configuration

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,32 @@ require "action_controller/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# load ENV variables from .env file if it exists
+env_file = File.expand_path("../../.env", __FILE__)
+if File.exist?(env_file)
+  require 'dotenv'
+  Dotenv.load! env_file
+end
+
+# load ENV variables from container environment if json file exists
+# see https://github.com/phusion/baseimage-docker#envvar_dumps
+env_json_file = "/etc/container_environment.json"
+if File.exist?(env_json_file)
+  env_vars = JSON.parse(File.read(env_json_file))
+  env_vars.each { |k, v| ENV[k] = v }
+end
+
+# default values for some ENV variables
+ENV['APPLICATION'] ||= "content-negotiation"
+ENV['HOSTNAME'] ||= "data.local"
+ENV['MEMCACHE_SERVERS'] ||= "memcached:11211"
+ENV['SITE_TITLE'] ||= "Content Resolver"
+ENV['LOG_LEVEL'] ||= "info"
+ENV['GITHUB_URL'] ||= "https://github.com/crosscite/content-negotiation"
+ENV['SEARCH_URL'] ||= "https://search.datacite.org/"
+ENV['CITEPROC_URL'] ||= "https://citation.datacite.org/format"
+ENV['TRUSTED_IP'] ||= "10.0.0.0/8"
+
 module ContentNegotiation
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -31,6 +57,7 @@ module ContentNegotiation
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
+    config.log_level = ENV['LOG_LEVEL'].to_sym
     config.lograge.enabled = true
     config.lograge.ignore_actions = ['HeartbeatController#index']
 
@@ -46,5 +73,8 @@ module ContentNegotiation
 
     # compress responses with deflate or gzip
     config.middleware.use Rack::Deflater
+
+    # Use memcached as cache store
+    config.cache_store = :dalli_store, nil, { expires_in: 7.days }
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,3 +33,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP']
