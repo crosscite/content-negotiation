858287c61616f23344071786dd961c60ea4efb26
Martin Fenner <mfenner@datacite.org>
2017-10-23 09:54:59 +0200

    cache datacite xml

--- app/models/metadata.rb
+++ app/models/metadata.rb
@@ -1,16 +1,28 @@
 class Metadata < Bolognese::Metadata
-  # fetching and parsing is an expensive step, so we cache using the crosscite format
+  # fetching and parsing is an expensive step, so we cache using the crosscite format, or raw xml
   def initialize(input: nil, from: nil, format: nil, **options)
     # don't cache if passing thru metadata or when in Test mode
-    return super(input: input, from: from, sandbox: options[:sandbox]) if from == format.to_s || %w(test).include?(Rails.env)
+    return super(input: input, from: from, sandbox: options[:sandbox]) if %w(test).include?(Rails.env)
 
-    cached_input = Rails.cache.read(input)
+    # passthru of DataCite or Crossref xml
+    if from == format.to_s
+      cached_input = Rails.cache.read("#{from}/#{input}")
 
-    if cached_input
-      super(input: cached_input, from: "crosscite")
+      if cached_input
+        super(input: cached_input, from: from)
+      else
+        super(input: input)
+        Rails.cache.write("#{from}/#{input}", self.raw, raw: true)
+      end
     else
-      super(input: input)
-      Rails.cache.write(input, self.crosscite, raw: true)
+      cached_input = Rails.cache.read("crosscite/#{input}")
+
+      if cached_input
+        super(input: cached_input, from: "crosscite")
+      else
+        super(input: input)
+        Rails.cache.write("crosscite/#{input}", self.crosscite, raw: true)
+      end
     end
   end
 end
