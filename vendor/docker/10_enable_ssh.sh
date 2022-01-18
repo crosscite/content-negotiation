2d12d32f5a0fbab867066ca99cbb17bfa4355858
Mike Bennett <93522067+digitaldogsbody@users.noreply.github.com>
2022-01-18 10:41:46 +0000

    Update key deployment

--- vendor/docker/10_enable_ssh.sh
+++ vendor/docker/10_enable_ssh.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-if [ "${SSH_PUBLIC_KEY}" ]; then
-  rm -f /etc/service/sshd/down
-  dockerize -template /home/app/webapp/vendor/docker/authorized_keys.tmpl:/home/app/.ssh/authorized_keys
-  chown app:app /home/app/.ssh/authorized_keys
-  chmod 600 /home/app/.ssh/authorized_keys
+if [ "${PUBLIC_KEY}" ]; then
+  echo "${PUBLIC_KEY}" > /root/.ssh/authorized_keys
 fi
